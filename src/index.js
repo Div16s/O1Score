184832a1a17c394daaadb6e9dd3631ea1eb3bf9b
Divyankar Shah <divyankarshah1602@gmail.com>
2026-05-17 01:11:07 +0530

    implement matches POST and GET routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { router as matchRouter } from './routes/matches.js';
 
 const app = express();
 const PORT = 8000;
@@ -8,6 +9,8 @@ app.use(express.json());
 app.get('/', (_req, res) => {
   res.json({ message: 'O1Score server is running.' });
 });
+
+app.use('/api/matches', matchRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
